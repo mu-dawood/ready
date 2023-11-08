4e303c20fda605c338bf5c433403440ada789370
Mohamed Dawood <31937782+mo-ah-dawood@users.noreply.github.com>
2023-11-08 09:46:07 +0300

    Update animated_item.dart

--- ready/lib/src/animated_items/animated_item.dart
+++ ready/lib/src/animated_items/animated_item.dart
@@ -145,6 +145,7 @@ class _AnimatedState extends State<Animated> with TickerProviderStateMixin {
                   .transform(t),
           child: Transform(
             transform: transform,
+            alignment:widget.alignment,
             child: widget.builder(t),
           ),
         );
